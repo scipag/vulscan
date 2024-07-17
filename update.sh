01d541c16726554aa21cfda9cd598e5ee5135727
Timo Pallach <timo@pallach.de>
2024-07-17 06:38:07 +0200

    Update update.sh
    I am using the nmap vulscan script also on FreeBSD and macOS.
    wget on these system is not installed in /usr/bin.
    Also bash is not always installed on these system.
    So I changed the update.sh script to make it more platform independend.

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 declare -a databases=("cve" "exploitdb" "openvas" "osvdb" "scipvuldb" "securityfocus"
 "securitytracker" "xforce")
 
 for DB in "${databases[@]}"; do
-    /usr/bin/wget https://www.computec.ch/projekte/vulscan/download/${DB}.csv
+    wget https://www.computec.ch/projekte/vulscan/download/${DB}.csv
     
     if [ -f ${DB}.csv.1 ]; then
         mv ${DB}.csv.1 ${DB}.csv
